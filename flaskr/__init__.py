f38f4cdbc8f3c1bde400e9b4f9a55d40bef72eca
Mike Woodhouse <mikewoodhouse@gmail.com>
2020-11-12 11:59:07 +0000

    hooked up simple basic static html

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.season import Season
 
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='../static',
+            static_url_path='')
 
 
-@app.route("/season")
+@app.route('/')
+def root():
+    print('**** IN root()) ****')
+    return app.send_static_file('index.html')
+
+
+@app.route("/seasons", methods=['GET'])
 def season():
-    return str(Season.get(1949))
+    print(request)
+    return jsonify(Season.get(1949)._asdict())
