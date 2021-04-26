5ff9ed2bf518fe362620b838a222c7acce661da3
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-04-26 14:13:05 +0100

    more cleaning up of files from first attempt

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from flask import render_template
-from models.season import Season
-from models.season_record import SeasonRecord
-from models.century import Century
 
 
 app = Flask(__name__,
@@ -15,23 +12,6 @@ app = Flask(__name__,
 @app.route('/index')
 def root():
     return render_template('index.html')
-
-
-@app.route("/seasons", methods=['GET'])
-def season():
-    return render_template('seasons.html', seasons=Season.all())
-
-
-@app.route("/season_record/<year>")
-def season_record(year):
-    recs = SeasonRecord.get(year)
-    has_balls = any(r.has_balls for r in recs)
-    return render_template('season_record.html', year=year, recs=recs, has_balls=has_balls)
-
-
-@app.route("/centuries")
-def centuries():
-    return render_template('centuries.html', tons=Century.all())
 
 
 if __name__ == "__main__":
