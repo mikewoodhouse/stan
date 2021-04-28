47cd19f78eb36475566cc905329634c0f4db7dac
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-04-28 11:48:35 +0100

    simple seasons (no database yet)

--- app.py
+++ app.py
@@ -14,5 +14,14 @@ def root():
     return render_template('index.html')
 
 
+@app.route('/seasons')
+def seasons():
+    data = [
+        {'year': 1949, 'played': 12, 'won': 4, 'lost': 7, 'drawn': 1, 'tie': 0, 'noresult': 0, 'maxpossiblegames': 12},
+        {'year': 1950, 'played': 14, 'won': 6, 'lost': 6, 'drawn': 2, 'tie': 0, 'noresult': 0, 'maxpossiblegames': 14},
+    ]
+    return render_template('seasons.html', data=data)
+
+
 if __name__ == "__main__":
     app.run()
