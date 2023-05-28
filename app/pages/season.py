1895fa5c036e0606b7e46f9c9673c8772c6db045
Mike Woodhouse <mikewoodhouse@gmail.com>
2023-05-28 12:03:56 +0100

    refactor: extract method: show_batting

--- app/pages/season.py
+++ app/pages/season.py
@@ -20,44 +20,35 @@ def show_season(db: sqlite3.Connection, year: int) -> None:
         players = Player.all(db)
         with ui.card():
             averages, also_batted = BattingAverage.for_year(db, year, min_innings)
-            for average in averages:
-                average.name = players[average.player_id].name
-            records = [asdict(row) for row in averages]
-            with ui.table(
-                rows=records,
-                columns=BattingAverage.table_cols(),
-                title=f"Batting (min {min_innings} innings)",
-            ).props("dense") as table:
-                table.add_slot(
-                    "body-cell-name",
-                    r"""
-                <td :props="props">
-                    <a :href="'/players/' + props.row.player_id">{{props.row.name}}</a>
-                </td>
-                """,
-                )
-            for average in also_batted:
-                average.name = players[average.player_id].name
-            records = [asdict(row) for row in also_batted]
-            columns = BattingAverage.table_cols()
-            with ui.table(rows=records, columns=columns, title="Also batted").props(
-                "dense"
-            ) as table:
-                table.add_slot(
-                    "body-cell-name",
-                    r"""
-                <td :props="props">
-                    <a :href="'/players/' + props.row.player_id">{{props.row.name}}</a>
-                </td>
-                """,
-                )
-                table.add_slot(
-                    "body-cell-position",
-                    r"""
-                <td>&nbsp;</td>
-                """,
-                )
+            show_batting(min_innings, players, averages, show_position=True)
+            show_batting(min_innings, players, also_batted, show_position=False)
         with ui.card():
             ui.label("bowl aves")
         with ui.card():
             ui.label("other stats")
+
+
+def show_batting(min_innings, players, averages, show_position=True):
+    for average in averages:
+        average.name = players[average.player_id].name
+    records = [asdict(row) for row in averages]
+    with ui.table(
+        rows=records,
+        columns=BattingAverage.table_cols(),
+        title=f"Batting (min {min_innings} innings)",
+    ).props("dense") as table:
+        table.add_slot(
+            "body-cell-name",
+            r"""
+                <td :props="props">
+                    <a :href="'/players/' + props.row.player_id">{{props.row.name}}</a>
+                </td>
+                """,
+        )
+        if not show_position:
+            table.add_slot(
+                "body-cell-position",
+                r"""
+            <td>&nbsp;</td>
+            """,
+            )
