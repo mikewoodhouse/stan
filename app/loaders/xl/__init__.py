762fee423e6e5e5f00529feff72c94ab760828c7
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-03-07 12:03:42 +0000

    XL partnerships - stub

--- app/loaders/xl/__init__.py
+++ app/loaders/xl/__init__.py
@@ -2,3 +2,4 @@ from .match_batting_importer import XlBattingImporter
 from .match_bowling_importer import XlBowlingImporter
 from .match_card import XlMatchCard
 from .match_importer import XlMatchImporter
+from .partnerships_importer import XlPartnershipsImporter
--- extract_xl_to_csv.py
+++ extract_xl_to_csv.py
@@ -3,8 +3,9 @@ from pathlib import Path
 
 from openpyxl import load_workbook
 
-from app.loaders.xl import XlBattingImporter, XlBowlingImporter, XlMatchImporter
-from app.types import Match, MatchBatting, MatchBowling
+from app.loaders.xl import XlBattingImporter  # XlPartnershipsImporter,
+from app.loaders.xl import XlBowlingImporter, XlMatchImporter
+from app.types import Match, MatchBatting, MatchBowling  # , Partnership
 
 
 def clear_csv_files(path: Path) -> None:
@@ -22,9 +23,15 @@ if __name__ == "__main__":
     clear_csv_files(path)
 
     for filepath in path.glob("*.xlsm"):
-        print(f"importing {filepath}")
-        workbook = load_workbook(str(filepath))
+        year = int(filepath.stem[-4:])
+        print(f"importing {filepath} for {year}")
+        workbook = load_workbook(str(filepath), data_only=True)
         XlMatchImporter(workbook, csv_path, "matches", Match).ingest()
         XlBattingImporter(workbook, csv_path, "match_batting", MatchBatting).ingest()
         XlBowlingImporter(workbook, csv_path, "match_bowling", MatchBowling).ingest()
+        # NOTE: partnerships are only needed if they aren't already in the database, which they were until 2022
+        # if year >= 2023:
+        #     XlPartnershipsImporter(
+        #         workbook, csv_path, "partnerships", Partnership
+        #     ).ingest()
     print("done!")
