1fddd195dab144cdbc60742cccbaee2959342c70
MIke Woodhouse <mikewoodhouse@gmail.com>
2025-01-11 18:29:19 +0000

    fix: include match year in hdr

--- app/pages/match.py
+++ app/pages/match.py
@@ -12,7 +12,7 @@ def show_match(db: sqlite3.Connection, match_id: int) -> None:
     bowling = MatchBowling.for_match_id(db, match_id)
     match: dict = [match.row_dict() for match in Match.for_id(db, match_id)][0]
 
-    hdr = f"{match['match_date']} vs {match['oppo']} ({match['venue']}) - {match['result']} - {match['bat_first']} {match['score_1']} {match['bat_second']} {match['score_2']}"
+    hdr = f"{match['date']} vs {match['oppo']} ({match['venue']}) - {match['result']} - {match['bat_first']} {match['score_1']} {match['bat_second']} {match['score_2']}"
 
     with ui.header(elevated=True).style("background-color: maroon"):
         ui.label(hdr).style("color: gold").style("font-size: 200%")
