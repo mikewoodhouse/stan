c885d3884c0c07de3d37ee275ee5143f01970b78
MIke Woodhouse <mikewoodhouse@gmail.com>
2025-02-14 15:22:06 +0000

    feat: dummy pages (for now)

--- app/pages/sidebar_menu.py
+++ app/pages/sidebar_menu.py
@@ -5,10 +5,12 @@ def sidebar() -> None:
     with ui.left_drawer() as sidebar:
         sidebar.props("width=100")
         with ui.column():
-            ui.link("Hundreds", "/hundreds")
             ui.link("Seasons", "/seasons")
             ui.link("Players", "/players")
             ui.link("Partnerships", "/partnerships/1")
             ui.link("Appearances", "/appearances")
             ui.label("Batting")
             ui.label("Bowling")
+            ui.label("Fielding")
+            ui.link("Hundreds", "/hundreds")
+            ui.label("Captains")
