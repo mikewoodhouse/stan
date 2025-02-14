237befd8e21e74bcf21d7e26af2ddf3c3d608fee
MIke Woodhouse <mikewoodhouse@gmail.com>
2025-02-14 15:21:50 +0000

    fix: appease linter

--- app/pages/__init__.py
+++ app/pages/__init__.py
@@ -8,3 +8,16 @@ from .player_list import show_player_list
 from .player_year import show_player_year
 from .season import show_season
 from .seasons import show_seasons
+
+__all__ = [
+    "show_appearances",
+    "hundreds_report",
+    "show_match",
+    "show_matches",
+    "show_partnerships",
+    "show_player",
+    "show_player_list",
+    "show_player_year",
+    "show_season",
+    "show_seasons",
+]
